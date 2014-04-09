1760e187463a39debc8e52d7e4b7f24f77aab05c
Prelang Builder <builder@prelang.com>
2014-04-09 21:16:06 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarDoctor::Application.config.secret_key_base = '29a4ee4a538e06902ed094fbc947b50723d45df0c1be0121b9ec6fcc456a75d008614e98bc60f3203bfed5cc33c62fb74f482ca3bae4221f29288922e09d0b15'
