cdab7eec8b45c43b213220ad2e979f1c5e6ae589
Prelang Builder <builder@prelang.com>
2014-04-09 21:17:10 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,5 +1,5 @@
 class SolutionsController < ApplicationController
-  before_action :set_solution, only: [:show, :edit, :update, :destroy]
+  before_action :set_solution, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /solutions
   # GET /solutions.json
